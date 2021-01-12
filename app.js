66cd5c2308140650483a0c4236c2630179f4884c
haljjagi <01dlwldnjs@gmail.com>
2021-01-13 00:20:46 +0900

    update

--- app.js
+++ app.js
@@ -14,10 +14,9 @@ function enlarge() {
 
 
 
-canvas.addEventListener("click", function(e) {
-    clearInterval(start);
-    const enlargeNose = setInterval(enlarge, 1);
-})
+canvas.addEventListener("click", onClick);
+canvas.addEventListener("click", play);
+
 
 ctx.drawImage(img, 718, 332, 484, 416);
 
@@ -25,4 +24,19 @@ function draw() {
     ctx.drawImage(img, 718, 332, 484, 416);
 }
 
+
 const start = setInterval(draw, 1);
+
+function onClick(e) {
+    clearInterval(start);
+    const enlargeNose = setInterval(enlarge, 1);
+    canvas.removeEventListener("click", onClick, false);
+}
+
+
+
+
+function play(e) { 
+    const audio = document.getElementById('audio_play'); 
+    audio.play(); 
+} 
