df7a6457bf973f55e75916b319bf28e2a7a3d432
haljjagi <01dlwldnjs@gmail.com>
2021-01-12 23:49:22 +0900

    update

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ const ctx = canvas.getContext("2d");
 const img = new Image();
 img.src = "kaneKo.png";
 const nose = new Image();
-nose.src = "KoTrans.png";
+nose.src = "Ko.png";
 let i = 0;
 function enlarge() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(img, 718, 332, 484, 416);
     ctx.drawImage(nose, 718-i/2, 332-i/2, 484+i, 416+i);
-    console.log("as");
     i+=2;
 }
 
-const enlargeNose = setInterval(enlarge, 1);
+
+
+canvas.addEventListener("click", function(e) {
+    clearInterval(start)
+    const enlargeNose = setInterval(enlarge, 1);
+})
+
+ctx.drawImage(img, 718, 332, 484, 416);
+
+function draw() {
+    ctx.drawImage(img, 718, 332, 484, 416);
+}
+
+const start = setInterval(draw, 1);
